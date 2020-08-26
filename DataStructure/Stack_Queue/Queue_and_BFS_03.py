e6a461309dbc69a0ed09c877e145b9caecc2cd83
YimJiYoung <chiy323@gmail.com>
2020-08-26 15:17:09 +0900

    Queue & Stack : Queue and BFS 03 <Pefect Squares>

---
+++ DataStructure/Stack_Queue/Queue_and_BFS_03.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    # Using Dynamic Programing
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        cntPerfectSquares = [float('inf')]*(n+1)
+        cntPerfectSquares[0] = 0
+        for i in range(1, n+1) :
+            sqrt = int(math.sqrt(i))
+            for j in range(sqrt, 0, -1):
+                cntPerfectSquares[i] = min(cntPerfectSquares[i], cntPerfectSquares[i - j*j] + 1)
+        return cntPerfectSquares[n]
+
+    #Using Static Dynamic Programming
+    # cntPerfectSquares = [0]
+    # def numSquares(self, n):
+    #     """
+    #     :type n: int
+    #     :rtype: int
+    #     """
+    #     length = len(Solution.cntPerfectSquares)
+    #     if length > n+1:
+    #         return Solution.cntPerfectSquares[n]
+        
+    #     for i in range(length, n+1) :
+    #         sqrt = int(math.sqrt(i))
+    #         cnt = float('inf')
+    #         for j in range(sqrt, 0, -1):
+    #             cnt = min(cnt, Solution.cntPerfectSquares[i - j*j] + 1)
+    #         Solution.cntPerfectSquares.append(cnt)
+    #     return Solution.cntPerfectSquares[n]
+
+    # Using BFS
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # perfect squares smallar than n
+        sqrt = int(math.sqrt(n))
+        perfectSquares = list(range(1, sqrt))
+
+        toCheck = {n}
+
+        while toCheck:
+
+        return 
+
