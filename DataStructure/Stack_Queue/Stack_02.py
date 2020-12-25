10e44d3e0da74309df11e9443df39c7cea2d3449
YimJiYoung <chiy323@gmail.com>
2020-12-25 16:41:04 +0900

    Chore

--- DataStructure/Stack_Queue/Stack_02.py
+++ DataStructure/Stack_Queue/Stack_02.py
@@ -1,3 +1,9 @@
+#<Valid Parentheses>
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+
 class Solution(object):
     def isValid(self, s):
         """
@@ -26,3 +32,19 @@ class Solution(object):
         if len(stack) != 0:
             return False
         return True
+
+#  Better solution
+#  class Solution:
+#     # @return a boolean
+#     def isValid(self, s):
+#         stack = []
+#         dict = {"]":"[", "}":"{", ")":"("}
+#         for char in s:
+#             if char in dict.values():
+#                 stack.append(char)
+#             elif char in dict.keys():
+#                 if stack == [] or dict[char] != stack.pop():
+#                     return False
+#             else:
+#                 return False
+#         return stack == []
