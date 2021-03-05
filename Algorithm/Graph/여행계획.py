a1f6f5e302a795e1f1f66d5d37f0873321c30162
YimJiYoung <chiy323@gmail.com>
2021-03-05 15:43:00 +0900

    Graph: 여행 계획 (서로소 집합)

---
+++ Algorithm/Graph/여행계획.py
@@ -0,0 +1,44 @@
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a == b:
+        return False
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+    return True
+
+
+def check_plan(parent, plan):
+    for i in range(m - 1):
+        if find_parent(parent, plan[i]) != find_parent(parent, plan[i + 1]):  # 연결되어 있지 않음
+            return False
+    return True
+
+
+n, m = map(int, input().split(' '))
+graph = []
+parent = [i for i in range(n)]
+
+for i in range(n):
+    row = list(map(int, input().split(' ')))
+    graph.append(row)
+    for j in range(n):
+        if graph[i][j] == 1:  # 노드가 연결된 경우
+            union_parent(parent, i, j)
+
+plan = list(map(int, input().split(' ')))
+
+if check_plan(parent, plan):
+    print('YES')
+else:
+    print('NO')
